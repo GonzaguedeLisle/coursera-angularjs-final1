be9074601adf2d8766d38626e8799d62b7e31f54
GonzaguedeLisle <gonzague.de.lisle@gmail.com>
2021-04-23 17:10:02 +0200

    'first end'

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+    //Module declaration
+    var module = angular.module('LunchCheck', []) ;
 
-LunchCheckController.$inject = ['$scope'];
+    //Controller declaration
+    module.controller('LunchCheckController', LunchCheckController);
 
-function LunchCheckController ($scope) {
-    $scope.checkQtity = function () {
-        alert("checkQtity");
-        console.log('checkQtity');
-    };
-}
-});
+    //Service injection to protect against minification
+    LunchCheckController.$inject = ['$scope'];
+
+    //Function evaluating the items
+    function LunchCheckController ($scope) {
+
+        //Checks if data has been entered, and displays message if not 
+        //If there is data, calls the function to calculate the amount
+        $scope.checkQtity = function () {
+            if ($scope.list) {
+                $scope.noData = "";
+                $scope.quantity = assessQtity ($scope.list);
+            } else {
+                $scope.quantity = "";
+                $scope.noData = "Please enter data first";
+            }
+        }
+
+        //Splits the string and returns the appropriate message
+        function assessQtity (string) {
+            var itemsArrayLength = (string.split(',')).length;
+            if (itemsArrayLength <= 3) {
+                return "Enjoy!";
+            } else {
+                return "Too much!";
+            }
+        }
+    }
+})();
